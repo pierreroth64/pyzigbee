992cbe33b7d40b77c389eb2a9b5a008f983ac33b
Pierre ROTH <pierre.roth@legrand.fr>
2015-06-03 14:17:15 +0200

    Cleanups in exceptions

--- pyzigbee/core/exceptions.py
+++ pyzigbee/core/exceptions.py
@@ -21,24 +21,6 @@ class PyZigBeeException(Exception):
     def __repr__(self):
         return self.msg
 
-class PyZigBeeAmbiguous(PyZigBeeException):
-    """Raised when definition of a data does not lead to unique result as it should"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeAmbiguous, self).__init__(msg=msg, detail=detail)
-
-class PyZigBeeNotAvailable(PyZigBeeException):
-    """Raised when a request is done on data  that is not available"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeNotAvailable, self).__init__(msg=msg, detail=detail)
-
-class PyZigBeeEntryAlreadyExists(PyZigBeeException):
-    """Raised when an attempt is made to add data that already exists and must be unique"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeEntryAlreadyExists, self).__init__(msg=msg, detail=detail)
-
 class PyZigBeeBadFormatError(PyZigBeeException):
     """Raised when bad format is encountered"""
 
@@ -50,12 +32,6 @@ class PyZigBeeResourceNotFound(PyZigBeeException):
 
     def __init__(self, msg, detail=None):
         super(PyZigBeeResourceNotFound, self).__init__(msg=msg, detail=detail)
-
-class PyZigBeeBadConfig(PyZigBeeException):
-    """Raised when a bad configuration is provided"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeBadConfig, self).__init__(msg=msg, detail=detail)
 
 class PyZigBeeBadArgument(PyZigBeeException):
     """Raised when a bad argument is provided to a method"""
@@ -75,35 +51,11 @@ class PyZigBeeFailed(PyZigBeeException):
     def __init__(self, msg, detail=None):
         super(PyZigBeeFailed, self).__init__(msg=msg, detail=detail)
 
-class PyZigBeeExceedLimit(PyZigBeeException):
-    """Raised when a system limit is reached"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeExceedLimit, self).__init__(msg=msg, detail=detail)
-
 class PyZigBeeOperationNotSupported(PyZigBeeException):
     """Raised when a operation is not supported"""
 
     def __init__(self, msg, detail=None):
         super(PyZigBeeOperationNotSupported, self).__init__(msg=msg, detail=detail)
-
-class PyZigBeeWarning(PyZigBeeException):
-    """Raised to warn the calling code"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeWarning, self).__init__(msg=msg, detail=detail)
-
-class PyZigBeeNotEnough(PyZigBeeException):
-    """Raised to warn not enough data"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeNotEnough, self).__init__(msg=msg, detail=detail)
-
-class PyZigBeeAborted(PyZigBeeException):
-    """Raised when an abort is requested"""
-
-    def __init__(self, msg, detail=None):
-        super(PyZigBeeAborted, self).__init__(msg=msg, detail=detail)
 
 class PyZigBeeDenied(PyZigBeeException):
     """Raised when an operation is requested but conditions required are not met"""
