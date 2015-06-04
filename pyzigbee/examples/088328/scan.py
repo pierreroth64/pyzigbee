848f5e7bc6960df6921ea4a144fbd4bb6fa15fb0
Pierre Roth <pierreroth@users.noreply.github.com>
2015-06-04 17:36:40 +0200

    Add user waning in example code

--- pyzigbee/examples/088328/scan.py
+++ pyzigbee/examples/088328/scan.py
@@ -4,6 +4,9 @@
 from contextlib import closing
 from pyzigbee.gateways.factory import GatewayFactory
 
+# Note that you may need to pass a configuration file to the GatewayFactory since
+# your low level device may be different from the default one. Please read the 
+# Configuration section of the documentation
 gateway = GatewayFactory().create_gateway(ref="088328")
 
 with closing(gateway.open()) as gateway:
