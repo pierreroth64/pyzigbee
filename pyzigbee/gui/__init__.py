159debc15508e3b691856041d7f128472e25754a
Pierre ROTH <pierre.roth@legrand.fr>
2015-06-08 11:05:01 +0200

    Remove GUI application (work in progress)

--- pyzigbee/gui/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2015 Legrand France
-# All rights reserved
-
-"""This package contains a GUI application running on top of the pyzigbee library"""
-
